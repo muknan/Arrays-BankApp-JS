9e42dea2e5d4a6853290b44162f8727551d47642
muknan <muk.msi@gmail.com>
2023-09-15 13:51:38 +0530

    Creating DOM elements

--- script.js
+++ script.js
@@ -61,17 +61,61 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = ''; //Empty out the element before inserting the html nodes to DOM
+  movements.forEach((mov, i) => {
+    const type = mov < 0 ? `withdrawal` : `deposit`;
+
+    const html = `<div class="movements__row">
+  <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+  <div class="movements__value">${mov}</div>
+</div>`;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, mov] of movements.entries()) {
+  if (mov < 0)
+    console.log(`Transaction (${i + 1}): You withdrew ${Math.abs(mov)}`);
+  else console.log(`Transaction (${i + 1}): You deposited ${mov}`);
+}
+
+console.log('\nFOR EACH STARTS HERE\n\n');
+
+// continue and break does not work on (foreach), it loops over entire array unlike (for of)
+movements.forEach((mov, i, arr) => {
+  if (mov < 0)
+    console.log(`Transaction (${i + 1}): You withdrew ${Math.abs(mov)}`);
+  else console.log(`Transaction (${i + 1}): You deposited ${mov}`);
+});
+
+
+// Map
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach((value, key, map) => {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log([...currenciesUnique]);
+currenciesUnique.forEach((value, _, set) => {
+  console.log(`${value}: ${value}`);
+});
+*/
 
 /////////////////////////////////////////////////
 
@@ -107,4 +151,16 @@ console.log([...arr, ...arr2]); // No mutation
 console.log(letters.join(' - '));
 // Split
 console.log(letters.join(' - ').split(' - '));
+
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting the last array element
+console.log(arr[arr.length - 1]);
+console.log(...arr.slice(-1));
+console.log(arr.at(-1)); // easier like slice but no need to spread and supports chaining
+
+console.log('mukul'.at(-1));
 */
