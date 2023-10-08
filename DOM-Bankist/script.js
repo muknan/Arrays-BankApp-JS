63eca6db9bb77cba927e404aff17a6e66dc00631
muknan <muk.msi@gmail.com>
2023-10-08 15:57:14 -0300

    OOPS in JS - first commit

--- DOM-Bankist/script.js
+++ DOM-Bankist/script.js
@@ -451,3 +451,17 @@ console.log(h1.parentElement.children);
   }
 });
 */
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log(e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log(e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
