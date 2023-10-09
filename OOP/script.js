332b6a783e7acaa1a3d27a1bb1d68155f4f30b0c
muknan <muk.msi@gmail.com>
2023-10-09 20:51:24 -0300

    Static methods, getter/setter

--- OOP/script.js
+++ OOP/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
+/*
+///////////////////////////////////////
+// ---------- Challenge 1 ---------- //
+///////////////////////////////////////
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.accelerate();
+car1.brake();
+car1.accelerate();
+*/
+
+/*
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -18,6 +46,14 @@ const meena = new Person('Meena', 1963);
 const gaurav = new Person('Gaurav', 1990);
 
 console.log(meena instanceof Person);
+
+Person.hey = function () {
+  console.log('Hey there 👋');
+  console.dir(this);
+};
+
+Person.hey();
+// mukul.hey() // Error
 
 // Prototypes
 Person.prototype.calcAge = function () {
@@ -58,50 +94,84 @@ console.log(x => x + 10);
 // ES6 classes
 // const PersonCl = class {}; or
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
-  age() {
+
+  // Instance methods
+  // Methods will be added to .prototype property
+  calcAge() {
     console.log(2037 - this.birthYear);
   }
   greet() {
-    console.log(`Hey ${this.firstName}`);
+    console.log(`Hey, ${this.fullName}`);
+  }
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else console.log('Not a full name');
+  }
+  get fullName() {
+    return this._fullName;
+  }
+
+  static hey() {
+    console.log('Hey there 👋');
+    console.dir(this);
   }
 }
 
-const tim = new PersonCl('Tim', 2011);
-tim.age();
+const tim = new PersonCl('Tim Cook', 2011);
+tim.calcAge();
+console.log(tim.age);
 console.log(tim.__proto__ === PersonCl.prototype);
+
+PersonCl.hey();
 
 // PersonCl.prototype.greet = function () {
 //   console.log(`Hey ${this.firstName}`);
 // };
 tim.greet();
 
-/*
-///////////////////////////////////////
-// ---------- Challenge 1 ---------- //
-///////////////////////////////////////
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
+const account = {
+  owner: 'Mukul',
+  movements: [1000, -200, 400, 500],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
 };
-
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(this.speed);
-};
-
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(this.speed);
-};
-
-const car1 = new Car('BMW', 120);
-const car2 = new Car('Mercedes', 95);
-
-car1.accelerate();
-car1.brake();
-car1.accelerate();
+console.log(account.latest);
+account.latest = 100;
+console.log(account.movements);
 */
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steve = Object.create(PersonProto);
+// steve.name = 'Steven';
+steve.birthYear = 2001;
+steve.calcAge();
+console.log(steve);
+
+const jesse = Object.create(PersonProto);
+jesse.init('Jesse', 1986);
+console.log(jesse);
+jesse.calcAge();
